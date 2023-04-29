c7ddb2255b5bae920dd2bdb90ee119bc447f6d63
Alex <alexndf@ciencias.unam.mx>
2023-04-29 17:20:25 -0600

    Se implementó un middleware para manejar errores y lanzarlos al usuario.

--- amigos-app/index.ts
+++ amigos-app/index.ts
@@ -1,6 +1,9 @@
-import express, { Express, Request, Response } from "express"
-import morgan from "morgan"
-import cors from 'cors'
+import express, { Express, Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import "express-async-errors";
+import "./src/utils/errors";
+import { errorHandler } from "./src/middleware/errors";
 
 const port = 8000
 const app: Express = express()
@@ -18,3 +21,6 @@ app.use(express.json())
 app.listen(port, () => {
   console.log(`Listening on port ${port} 🚀🚀🚀`)
 });
+
+// Error middleware, must be last line in index.ts
+app.use(errorHandler);
