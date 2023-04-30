e542d4d5fc759baf947968dd75f5929d49791a31
Alex Nakamura <72317263+AlexNakamuraDF@users.noreply.github.com>
2023-04-30 16:05:58 -0600

    Jwt auth (#3)
    * Se agregaron tokens de validación JWT y un sistema elemental de inicio de sesión
    
    * Se agregaron consultas al login y la autenticación
    
    * Se agregaron nuevos errores a la autenticación
    
    * Se re-estructuró el código del login y de la autenticación
    
    * Se cambió método de exportación para loginRouter y se cambió dotenv a que solamente fuera una dependencia de desarrollo

--- amigos-app/index.ts
+++ amigos-app/index.ts
@@ -4,23 +4,32 @@ import cors from "cors";
 import "express-async-errors";
 import "./src/utils/errors";
 import { errorHandler } from "./src/middleware/errors";
+import { loginRouter } from "./src/login/login.router";
+require("dotenv").config();
 
-const port = 8000
-const app: Express = express()
+const port = process.env.PORT;
+const app: Express = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send("bonjour!")
+app.get("/", (req: Request, res: Response) => {
+    res.send("bonjour!");
 });
 
 // Middlewares
-app.use(morgan('combined'))
-app.use(cors())
-app.use(express.json())
-
+app.use(morgan("combined"));
+app.use(cors());
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port} 🚀🚀🚀`)
+    console.log(`Listening on port ${port} 🚀🚀🚀`);
 });
+
+// Routers
+app.use("/login", loginRouter);
 
 // Error middleware, must be last line in index.ts
 app.use(errorHandler);
