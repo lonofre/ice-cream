6cfd05b264cd2673229244f4bff207acf25b6c7e
noe-suaste <suaste@ciencias.unam.mx>
2023-05-01 17:19:26 -0600

    Se modifico endpoint para obtener productos por categoria en query y se cambio el manejo de errores del router al service

--- amigos-app/src/product/product.service.ts
+++ amigos-app/src/product/product.service.ts
@@ -1,4 +1,5 @@
 import { db } from "../utils/db.server"
+import { APIError, HttpErrorCode } from "../utils/errors";
 
 type Product = {
   id: number;
@@ -12,64 +13,96 @@ type Product = {
 
 // Get all products from the database 
 export const getAllProducts = async (): Promise<Product[]> => {
-    return db.product.findMany({
-        select: {
-            id: true, 
-            name: true, 
-            description: true, 
-            image: true,
-            price: true,
-            categoryId: true,
-        },
-    });
+    try{
+        return db.product.findMany({
+            select: {
+                id: true, 
+                name: true, 
+                description: true, 
+                image: true,
+                price: true,
+                categoryId: true,
+            },
+        });
+    }catch(errror){
+        throw new APIError(
+            "Failed to get products",
+            HttpErrorCode.INTERNAL_SERVER_ERROR,
+            null
+        )
+    }
 };
 
 // Get a product by id 
 export const getProductById = async (id: number): Promise<Product | null> => {
-    return db.product.findUnique({
-        where: {
-          id,
-        },
-      });
+    try{
+        return db.product.findUnique({
+            where: {
+            id,
+            },
+        });
+    }catch(errror){
+        throw new APIError(
+            "Failed to get products by id",
+            HttpErrorCode.INTERNAL_SERVER_ERROR,
+            null
+        )
+    }
 }
 
 // Get a product by category
 export const getProductsByCategory = async (categoryName: string): Promise<Product[]> => {
-    return db.product.findMany({
-      where: {
-        category: {
-          name: categoryName,
+    try {
+      const products = await db.product.findMany({
+        where: {
+          category: {
+            name: categoryName,
+          },
         },
-      },
-      include: {
-        category: true,
-      },
-    });
-
-  }
+        include: {
+          category: true,
+        },
+      });
+      return products;
+    } catch (error) {
+      throw new APIError(
+        "Failed to fetch products by category",
+        HttpErrorCode.INTERNAL_SERVER_ERROR,
+        error,
+      );
+    }
+  };
 
 // Create a new product and save it to the database
 export const createProduct = async (
     product: Omit<Product, "id">
   ): Promise<Product> => {
-    const { name, description, image, price, categoryId } = product;
-    return db.product.create({
-      data: {
-        name,
-        description,
-        image, 
-        price, 
-        categoryId,
-      },
-      select: {
-        id: true,
-        name: true,
-        description: true,
-        image: true, 
-        price: true, 
-        categoryId: true,
-      },
-    });
+    try{
+        const { name, description, image, price, categoryId } = product;
+        return db.product.create({
+        data: {
+            name,
+            description,
+            image, 
+            price, 
+            categoryId,
+        },
+        select: {
+            id: true,
+            name: true,
+            description: true,
+            image: true, 
+            price: true, 
+            categoryId: true,
+        },
+        });
+    }catch(errror){
+        throw new APIError(
+            "Failed to create and save product on database",
+            HttpErrorCode.INTERNAL_SERVER_ERROR,
+            null
+        )
+    }
 };
   
 // Update an existing product in the database
@@ -77,36 +110,53 @@ export const updateProductById = async (
     product: Omit<Product, "id">,
     id: number
   ): Promise<Product> => {
-    const { name, description, image, price, categoryId } = product;
-    return db.product.update({
-      where: {
-        id,
-      },
-      data: {
-        name,
-        description,
-        image, 
-        price, 
-        categoryId,
-      },
-      select: {
-        id: true,
-        name: true,
-        description: true,
-        image: true, 
-        price: true, 
-        categoryId: true,
-      },
-    });
+    try{
+        const { name, description, image, price, categoryId } = product;
+        return db.product.update({
+        where: {
+            id,
+        },
+        data: {
+            name,
+            description,
+            image, 
+            price, 
+            categoryId,
+        },
+        select: {
+            id: true,
+            name: true,
+            description: true,
+            image: true, 
+            price: true, 
+            categoryId: true,
+        },
+        });
+    }catch(errror){
+        throw new APIError(
+            "Failed to update product",
+            HttpErrorCode.INTERNAL_SERVER_ERROR,
+            null
+        )
+    }
 };
   
 // Delete a product from the database
-export const deleteProduct = async (id: number): Promise<void> => {
-    await db.product.delete({
-      where: {
-        id,
-      },
-    });
+export const deleteProduct = async (id: number): Promise<string> => {
+    try{
+        await db.product.delete({
+        where: {
+            id,
+        },
+        });
+        return "Product deleted succesfully";
+    }catch(errror){
+        throw new APIError(
+            "Failed to delete product",
+            HttpErrorCode.INTERNAL_SERVER_ERROR,
+            null
+        )
+    }
 };
 
 
