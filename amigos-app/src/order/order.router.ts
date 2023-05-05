9302155ea51ca7c3a672e74838ea159500ad8f03
Juan13Ju <juan13ubaldo@gmail.com>
2023-05-05 09:03:41 -0600

    Agregue update de una orden

--- amigos-app/src/order/order.router.ts
+++ amigos-app/src/order/order.router.ts
@@ -17,10 +17,11 @@ import * as OrderItemService from "../order-item/order-item.service"
 
 export const orderRouter = express.Router();
 
-orderRouter.get("/order/:id", async(request : Request, response : Response) => {
+// Returns a list of the items ordered
+orderRouter.get("/order/:id", async (request: Request, response: Response) => {
     const id: number = parseInt(request.params.id, 10);
     const order = await OrderService.getOrderById(id);
-    if(order){
+    if (order) {
         return response.status(200).json(order);
     }
 
@@ -32,44 +33,95 @@ orderRouter.get("/order/:id", async(request : Request, response : Response) => {
 });
 
 orderRouter.post("/order",
-            // Validates sessionId exists 
-            body("seesionId").custom(async(sessionId) => {
-                const session: Session | null = await SessionService.validateSessionId(sessionId)
-                if(session){
-                    return true;
-                }
+    // Validates sessionId exists 
+    body("seesionId").custom(async (sessionId) => {
+        const session: Session | null = await SessionService.validateSessionId(sessionId)
+        if (session) {
+            return true;
+        }
+        return false;
+    }),
+    // Validates every OrderItem is valid
+    body("orderItems").custom(async (orderItems) => {
+        for (let i = 0; i < orderItems.length; i++) {
+            let productId: number = orderItems[i].productId
+            let product: Product | null = await OrderItemService.validateOrderItem(productId)
+            if (product == null) {
                 return false;
-            }),
-            // Validates every OrderItem is valid
-            body("orderItems").custom(async(orderItems) => {
-                for(let i = 0; i < orderItems.length; i++){
-                    let productId: number = orderItems[i].productId
-                    let product: Product | null = await OrderItemService.validateOrderItem(productId)
-                    if(product == null){
-                        return false;
-                    }
-                }
-                return true;
-            }),
-            // Creates the order
-            async(request: Request, response: Response) => {
-    const errors = validationResult(request);
-    if(!errors.isEmpty()){
-        throw new APIError(
-            "Verify the data and try again",
-            HttpErrorCode.BAD_REQUEST,
-            null
-        );
-    }
+            }
+        }
+        return true;
+    }),
+    // Creates the order
+    async (request: Request, response: Response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            throw new APIError(
+                "Verify the data and try again",
+                HttpErrorCode.BAD_REQUEST,
+                null
+            );
+        }
 
-    const {sessionId, orderItems} = request.body;
+        const { sessionId, orderItems } = request.body;
 
 
-    const savedOrder = await OrderService.createOrder({
-        sessionId,
-    }, orderItems);
+        const savedOrder = await OrderService.createOrder({
+            sessionId,
+        }, orderItems);
 
-    return response.status(201).json(savedOrder);
-});
+        return response.status(201).json(savedOrder);
+    });
 
-// TODO : update orderItem, method only adds an item to the order
+
+orderRouter.put("/order/:id",
+    // Validates every OrderItem is valid
+    body("orderItems").custom(async (orderItems) => {
+        for (let i = 0; i < orderItems.length; i++) {
+            let productId: number = orderItems[i].productId
+            let product: Product | null = await OrderItemService.validateOrderItem(productId)
+            if (product == null) {
+                return false;
+            }
+        }
+        return true;
+    }),
+    // Adds the new items to the order
+    async (request: Request, response: Response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            throw new APIError(
+                "Verify the data and try again",
+                HttpErrorCode.BAD_REQUEST,
+                null
+            );
+        }
+        const orderId = parseInt(request.params.id, 10);
+        const order = await OrderService.getOrderById(orderId);
+
+        if (order == null) {
+            throw new APIError(
+                "Order not found",
+                HttpErrorCode.NOT_FOUND,
+                null
+            );
+        }
+
+        const {orderItems} = request.body;
+
+        for(let i = 0; i < orderItems.length; i++){
+            let productId: number = parseInt(orderItems[i].productId, 10);
+            let quantity: number = parseInt(orderItems[i].quantity, 10);
+            OrderItemService.createOrderItem({
+                orderId,
+                productId,
+                quantity,
+                "comments": ""
+            })
+        }
+
+        return response.status(201).json({
+            "msg":"Items added succesfully"
+        })
+
+    });
